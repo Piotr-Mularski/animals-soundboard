cd1a0573450da87c3f06ec95beaefb7a141f879d
Piotr Mularski <piomul6473@gmail.com>
2018-09-15 21:46:49 +0200

    initial commit

---
+++ assets/scripts/main.js
@@ -0,0 +1,35 @@
+const animals = document.querySelectorAll('.animal');
+
+animals.forEach(animal => {
+	animal.addEventListener('click', function(e) {
+		const key = this.dataset["key"];
+		playAudio(key);
+	});
+	animal.addEventListener('transitionend', function(e) {
+		if(e.propertyName !== 'transform') {
+			return;
+		} else {
+			console.log(this)
+			this.classList.remove('animal--playing');
+		}
+	})
+});
+
+
+
+window.addEventListener('keypress', (e) => {
+	playAudio(e.key);
+});
+
+const playAudio = (key) => {
+	const audio = document.querySelector(`audio[data-key="${key}"]`);
+	const animal = document.querySelector(`.animal[data-key=${key}]`);
+	if(!audio) {
+		return;
+	} else {
+		audio.currentTime = 0; //so we can rewind every time we click
+		audio.src = audio.currentSrc; //because there's multiple sources for the audio, and the audio is playing from the src property
+		audio.play();
+		animal.classList.add('animal--playing');
+	}
+}
